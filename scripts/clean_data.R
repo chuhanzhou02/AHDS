ac2fcba1ce7f1792ddfeae8d8999254ce5795fe2
chuhanzhou02 <kv24343@bristol.ac.uk>
2024-11-30 17:49:15 +0000

    script

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(tidytext)
+library(SnowballC)
 
-data <- read_tsv("/home/zch/AHDS_project/clean/articles.tsv", col_names = c("PMID", "Year", "Title"))
+data <- read_tsv("clean/articles.tsv", col_names = c("PMID", "Year", "Title"))
 
 cleaned_data <- data %>%
   unnest_tokens(word, Title) %>%                           
@@ -12,12 +13,18 @@ cleaned_data <- data %>%
   filter(!str_detect(word, "^[[:punct:]]+$")) %>%          
   mutate(word = SnowballC::wordStem(word)) %>%             
   group_by(PMID, Year) %>%                                 
-  summarise(Cleaned_Title = paste(word, collapse = " "),   
-            .groups = "drop")
+  summarise(
+    Cleaned_Title = str_squish(paste(word, collapse = " ")), 
+    .groups = "drop"
+  ) %>%
+  arrange(Year, PMID)                                       
 
-output_file <- "/home/zch/AHDS_project/clean/article_clean.tsv"
+output_file <- "clean/article_clean.tsv"
 
 dir.create(dirname(output_file), showWarnings = FALSE, recursive = TRUE)
 
-write_tsv(cleaned_data, output_file)
+write_tsv(cleaned_data, output_file, na = "NA", col_names = TRUE)
+
+cat("Data cleaning completed. File saved to:", output_file, "\n")
+
 
